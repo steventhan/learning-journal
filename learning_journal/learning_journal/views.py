54682633b6312cb942d819be9c02beb2b5ad7bef
zachrickert <zachrickert@gmail.com>
2016-08-22 16:13:16 -0700

    Add edit-entry, single-entry, new_entry.

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -9,6 +9,23 @@ def home_page(request):
     return Response(imported_text)
 
 
+def view_entry(request):
+    imported_text = open(os.path.join(HERE + '/static/', 'single-entry.html')).read()
+    return Response(imported_text)
+
+
+def new_entry(request):
+    imported_text = open(os.path.join(HERE + '/static/', 'new-entry.html')).read()
+    return Response(imported_text)
+
+
+def edit_entry(request):
+    imported_text = open(os.path.join(HERE + '/static/', 'edit-entry.html')).read()
+    return Response(imported_text)
+
+
 def includeme(config):
     config.add_view(home_page, route_name='home')
-    
+    config.add_view(view_entry, route_name='single-entry')
+    config.add_view(new_entry, route_name='new-entry')
+    config.add_view(edit_entry, route_name='edit-entry')
