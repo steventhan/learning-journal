6e63ddca1e6f8d788ef262e861512b96c9b8a445
Steven Than <steventhan11@gmail.com>
2016-08-22 22:05:14 -0700

    finished with routes and templating

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -1,30 +1,32 @@
-from pyramid.response import Response
 from pyramid.view import view_config
-import os
-
-HERE = os.path.dirname(__file__)
 
 
-@view_config(route_name='home', renderer='templates/home.jinja2')
-def home_page(request):
-    return []
+@view_config(
+    route_name='home', renderer='learning_journal:templates/home.html'
+)
+def home_view(request):
+    return {}
 
 
-@view_config(route_name='detail', renderer='templates/home.jinja2')
-def detail_view(request):
-    return {"content": "This was transformed into an HTTP response"}
-
-
-def view_entry(request):
-    imported_text = open(os.path.join(HERE + '/static/', 'single-entry.html')).read()
-    return Response(imported_text)
-
-
+@view_config(
+    route_name='new-entry',
+    renderer='learning_journal:templates/new-entry.html'
+)
 def new_entry(request):
-    imported_text = open(os.path.join(HERE + '/static/', 'new-entry.html')).read()
-    return Response(imported_text)
+    return {}
 
 
+@view_config(
+    route_name='single-entry',
+    renderer='learning_journal:templates/single-entry.html'
+)
+def single_entry(request):
+    return {}
+
+
+@view_config(
+    route_name='edit-entry',
+    renderer='learning_journal:templates/edit-entry.html'
+)
 def edit_entry(request):
-    imported_text = open(os.path.join(HERE + '/static/', 'edit-entry.html')).read()
-    return Response(imported_text)
+    return {}
