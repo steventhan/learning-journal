5af0d5528d49f8d6514b308c3a6be897198a37a2
Steven Than <steventhan11@gmail.com>
2016-08-22 21:02:32 -0700

    test

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -1,12 +1,18 @@
-from pyramid.response import Response 
+from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 HERE = os.path.dirname(__file__)
 
 
+@view_config(route_name='home', renderer='templates/home.jinja2')
 def home_page(request):
-    imported_text = open(os.path.join(HERE + '/static/', 'index.html')).read()
-    return Response(imported_text)
+    return []
+
+
+@view_config(route_name='detail', renderer='templates/home.jinja2')
+def detail_view(request):
+    return {"content": "This was transformed into an HTTP response"}
 
 
 def view_entry(request):
@@ -22,10 +28,3 @@ def new_entry(request):
 def edit_entry(request):
     imported_text = open(os.path.join(HERE + '/static/', 'edit-entry.html')).read()
     return Response(imported_text)
-
-
-def includeme(config):
-    config.add_view(home_page, route_name='home')
-    config.add_view(view_entry, route_name='single-entry')
-    config.add_view(new_entry, route_name='new-entry')
-    config.add_view(edit_entry, route_name='edit-entry')
