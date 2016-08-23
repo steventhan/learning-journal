1b15614ba96c5c27cc941f25fa2ce3d52585288b
Steven Than <steventhan11@gmail.com>
2016-08-22 23:28:32 -0700

    readme, ready for heroku

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -5,6 +5,7 @@ from pyramid.view import view_config
     route_name='home', renderer='learning_journal:templates/home.html'
 )
 def home_view(request):
+    """Render home.html at '/'"""
     return {}
 
 
@@ -13,6 +14,7 @@ def home_view(request):
     renderer='learning_journal:templates/new-entry.html'
 )
 def new_entry(request):
+    """Render new-entry.html at '/new-entry'"""
     return {}
 
 
@@ -21,6 +23,7 @@ def new_entry(request):
     renderer='learning_journal:templates/single-entry.html'
 )
 def single_entry(request):
+    """Render single-entry.html at '/journal/12345'"""
     return {}
 
 
@@ -29,4 +32,5 @@ def single_entry(request):
     renderer='learning_journal:templates/edit-entry.html'
 )
 def edit_entry(request):
+    """Render edit-entry.html at '/journal/12345/edit-entry'"""
     return {}
