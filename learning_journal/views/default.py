5882fc93b2883e6649086c190092cb59fec21559
Steven Than <steventhan11@gmail.com>
2016-08-30 10:00:09 -0700

    fix entry order

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -13,7 +13,9 @@ def home_view(request):
         body = request.POST['body']
         new_entry = Entry(title=title, body=body)
         request.dbsession.add(new_entry)
-    journal_entries = request.dbsession.query(Entry).all()
+    journal_entries = request.dbsession.query(Entry).order_by(
+            Entry.creation_date.desc()
+    ).all()
     return {
         'title': 'Home',
         'journal_entries': journal_entries
